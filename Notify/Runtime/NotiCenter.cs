ad6b42aee04ce966260b48afa0945811b4d2198c
Lautaro <Lautaro.arino@gmail.com>
2021-01-18 11:16:20 +0100

    Version 1.0.0

---
+++ Notify/Runtime/NotiCenter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Notify
+{
+    public class NotiCenter
+    {
+        static NotiCenter instance = new NotiCenter();
+        Dictionary<(Type, string),object> notificationTypes = new Dictionary<(Type, string), object>();
+
+        static bool Match<T>((Type, string) key, string title)
+        {
+            return (key.Item1 == typeof(T) && key.Item2 == title);
+        }
+
+        public static void Notify<T>(T context, string title)
+        {
+            var notificationType = instance.notificationTypes.Where(s => Match<T>(s.Key, title)).
+            FirstOrDefault().Value as NotificationType<T>;
+
+            if (notificationType != null)
+                notificationType.subscribers.Invoke(context);
+        }
+
+        public static void Unsubscribe<T>(UnityAction<T> callback, string title = "")
+        {
+            NotificationType<T> notificationType = instance.notificationTypes.FirstOrDefault(s => Match<T>(s.Key, title)).Value as NotificationType<T>;
+            if (notificationType != null)
+            {
+                notificationType.subscribers.RemoveListener(callback);
+            }
+        }
+
+        public static void Subscribe<T>(UnityAction<T> callback, string title = "" )
+        {
+            NotificationType<T> notificationType;
+
+            if (instance.notificationTypes.Any(s => Match<T>(s.Key, title)))
+            {
+                notificationType = instance.notificationTypes.First(s => Match<T>(s.Key, title)).Value as NotificationType<T>;
+            }
+            else
+            {
+                notificationType = new NotificationType<T>( title);
+                instance.notificationTypes.Add( (typeof(T),title),notificationType)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           ;
+            }
+            notificationType.subscribers.AddListener(callback);
+        }
+    }
+    class NotificationType<T>
+    {
+        internal Type Type;
+        internal readonly string Title;
+        internal UnityEvent<T> subscribers = new UnityEvent<T>();
+
+        public NotificationType(string title)
+        {  
+            Title = title;
+        }
+    }
+}
