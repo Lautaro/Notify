13858c66c83f314dc5b9eb75b4e558dbe3907c1c
Lautaro <Lautaro.arino@gmail.com>
2021-01-18 11:43:27 +0100

    Renamed repo

---
+++ Notifyer/Runtime/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lautaro
+{
+    public class Notification<T> 
+    {
+        public string Topic { get; set; }
+        public T Context { get; set; }
+        public Notification(T context, string topic = "")
+        {
+            Context = context;
+            Topic = topic;
+        }
+    }
+}
