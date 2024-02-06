4777bc44c60f34b2d7c4366a987d6ec3a5a4c011
Lautaro Ariño <Lautaro.arino@gmail.com>
2024-02-06 12:07:52 +0100

    Removed mistake file

--- Runtime/NotifyerEvents.cs
+++
@@ -1,3 +0,0 @@
-﻿public class OpenMenu : NotifyerEventBase<UIMenus> { };
-public class ApplicationStateChanged : NotifyerEventBase<ApplicationStates> { }; // Notify only from the GameManager
-public class InGamePlayStateChanged : NotifyerEventBase<InGamePlayStates> { };
